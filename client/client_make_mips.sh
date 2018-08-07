fa2d10c0588a48b51e6c37ebb89225f87d4f4a5c
jun384668960 <Donyj_ljj@163.com>
2018-08-07 11:54:04 +0800

    添加君正mips编译脚本

---
+++ client/client_make_mips.sh
@@ -0,0 +1,33 @@
+#! /bin/sh
+cd miniupnpc-1.9.20150206
+make clean
+make -f MakefileMips
+cd ..
+
+cp udt4.11/src/*.cpp pjproject-2.3/pjnath/src/pjnath
+
+cd pjproject-2.3
+chmod +x configure
+chmod +x aconfigure
+make clean
+DIR="$( cd "$( dirname "$0"  )" && pwd  )"
+./configure --prefix=$DIR --host=mips-linux-gnu CC=mips-linux-uclibc-gnu-gcc --enable-epoll
+
+cd pjlib/build/
+make 
+cd ../../
+
+cd pjlib-util/build
+make
+cd ../../
+
+cd pjnath/build
+make
+cd ../../
+
+rm -f lib/*.a
+cp -f pjlib/lib/libpj-mips-unknown-linux-gnu.a lib
+cp -f pjlib-util/lib/libpjlib-util-mips-unknown-linux-gnu.a lib
+cp -f pjnath/lib/libpjnath-mips-unknown-linux-gnu.a lib
+
+echo "gss make ok"
