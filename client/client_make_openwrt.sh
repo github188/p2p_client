4e1ac698d3a69ee19a7c1d6c09cea865470419e6
jun384668960 <Donyj_ljj@163.com>
2018-08-07 15:28:06 +0800

    需要添加-lpthread -ldl（不影响其他平台） openwrt_mpis编译脚本

---
+++ client/client_make_openwrt.sh
@@ -0,0 +1,33 @@
+#! /bin/sh
+cd miniupnpc-1.9.20150206
+make clean
+make -f MakefileOpenwrt
+cd ..
+
+cp udt4.11/src/*.cpp pjproject-2.3/pjnath/src/pjnath
+
+cd pjproject-2.3
+chmod +x configure
+chmod +x aconfigure
+make clean
+DIR="$( cd "$( dirname "$0"  )" && pwd  )"
+./configure --prefix=$DIR --host=mipsel-openwrt-linux CC=mipsel-openwrt-linux-gcc --enable-epoll
+
+cd pjlib/build/
+make 
+cd ../../
+
+cd pjlib-util/build
+make
+cd ../../
+
+cd pjnath/build
+make
+cd ../../
+
+rm -f lib/*.a
+cp -f pjlib/lib/libpj-mips-unknown-linux-gnu.a lib
+cp -f pjlib-util/lib/libpjlib-util-mipsel-openwrt-linux-gnu.a lib
+cp -f pjnath/lib/libpjnath-mipsel-openwrt-linux-gnu.a lib
+
+echo "gss make ok"
