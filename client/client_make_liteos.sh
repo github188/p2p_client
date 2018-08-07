698b9edf8f2e021df469d32722d0b3b1d022c28b
jun384668960 <Donyj_ljj@163.com>
2018-08-07 15:10:04 +0800

    liteos arm-hisiv500-linux-gcc编译脚本--说还要添加东西，后续更新

---
+++ client/client_make_liteos.sh
@@ -0,0 +1,33 @@
+#! /bin/sh
+cd miniupnpc-1.9.20150206
+make clean
+make -f MakefileLiteos
+cd ..
+
+cp udt4.11/src/*.cpp pjproject-2.3/pjnath/src/pjnath
+
+cd pjproject-2.3
+chmod +x configure
+chmod +x aconfigure
+make clean
+DIR="$( cd "$( dirname "$0"  )" && pwd  )"
+./configure --prefix=$DIR --host=arm-hisiv500-linux --enable-epoll
+
+cd pjlib/build/
+make 
+cd ../../
+
+cd pjlib-util/build
+make
+cd ../../
+
+cd pjnath/build
+make
+cd ../../
+
+rm -f lib/*.a
+cp -f pjlib/lib/libpj-arm-hisiv500-linux-gnu.a lib
+cp -f pjlib-util/lib/libpjlib-util-arm-hisiv500-linux-gnu.a lib
+cp -f pjnath/lib/libpjnath-arm-hisiv500-linux-gnu.a lib
+
+echo "gss make ok"
