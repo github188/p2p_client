0ddd7d57770419ab4f2968f78052abb2c6151381
jun384668960 <Donyj_ljj@163.com>
2018-09-12 10:22:51 +0800

    add goke make shell

---
+++ client/client_make_goke.sh
@@ -0,0 +1,34 @@
+#! /bin/sh
+cd miniupnpc-1.9.20150206
+make clean
+make -f MakefileGoke
+cd ..
+
+cp udt4.11/src/*.cpp pjproject-2.3/pjnath/src/pjnath
+
+cd pjproject-2.3
+chmod +x configure
+chmod +x aconfigure
+make clean
+DIR="$( cd "$( dirname "$0"  )" && pwd  )"
+export LITEOS_BUILD=FALSE
+./configure --prefix=$DIR --host=arm-goke-linux CC=arm-goke-linux-uclibcgnueabi-gcc --enable-epoll CXXFLAGS=-DPJ_ARM_MIPS
+
+cd pjlib/build/
+make 
+cd ../../
+
+cd pjlib-util/build
+make
+cd ../../
+
+cd pjnath/build
+make
+cd ../../
+
+rm -f lib/*.a
+cp -f pjlib/lib/libpj-arm-goke-linux-gnu.a lib
+cp -f pjlib-util/lib/libpjlib-util-arm-goke-linux-gnu.a lib
+cp -f pjnath/lib/libpjnath-arm-goke-linux-gnu.a lib
+
+echo "gss make ok"
---
+++ client/sdk/p2pdemo/gcclib_goke.sh
@@ -0,0 +1,28 @@
+rm -f p2p-goke.a
+
+mkdir pjnath_obj
+cd pjnath_obj
+ar x ../../../pjproject-2.3/lib/libpjnath-arm-goke-linux-gnu.a
+cd ..
+
+mkdir miniupnp_obj
+cd miniupnp_obj
+ar x ../../../miniupnpc-1.9.20150206/libminiupnpc.a
+cd ..
+
+mkdir pjlib_obj
+cd pjlib_obj
+ar x ../../../pjproject-2.3/lib/libpj-arm-goke-linux-gnu.a
+cd ..
+
+mkdir pjlib-util_obj
+cd pjlib-util_obj
+ar x ../../../pjproject-2.3/lib/libpjlib-util-arm-goke-linux-gnu.a
+cd ..
+
+ar rc p2p-goke.a ./pjnath_obj/*.o ./miniupnp_obj/*.o ./pjlib_obj/*.o ./pjlib-util_obj/*.o
+
+rm -rf pjnath_obj
+rm -rf miniupnp_obj
+rm -rf pjlib_obj
+rm -rf pjlib-util_obj
